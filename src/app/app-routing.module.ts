fa465d455e8e36bf8128bb130bd6090439c1148f
SaiNaresh <Thalupuru.KamaSainaresh@capita.co.uk>
2020-01-23 01:11:32 +0530

    angular springboot okta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OktaCallbackComponent } from '@okta/okta-angular';
+import { CarListComponent } from './car-list/car-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'implicit/callback',
+    component: OktaCallbackComponent
+  },
+  {path:'cars',component:CarListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarListComponent } from './car-list/car-list.component';
+import { OktaAuthModule } from '@okta/okta-angular';
+import { AuthInterceptor } from './shared/car/okta/auth.interceptor';
+
+const config = {
+  issuer: 'https://dev-702316.okta.com/oauth2/default',
+  redirectUri: 'http://localhost:4200/implicit/callback',
+  clientId: '0oa11guviXPe3iraV4x6'
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    OktaAuthModule.initAuth(config)
   ],
-  providers: [],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
